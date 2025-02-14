cc0bd46ee4988d6ce76bea1e63a39b18457f9171
James <james.jolly@okstate.edu>
2025-02-13 20:41:29 -0600

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by James Jolly on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesTests/NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesTests.swift
+//  NotesTests
+//
+//  Created by James Jolly on 2/13/25.
+//
+
+import Testing
+@testable import Notes
+
+struct NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
